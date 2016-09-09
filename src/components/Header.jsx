f3ae559b7ee1bf9171457fc9f4a178524968f344
Wenqian Guo <wnqnguo@gmail.com>
2016-09-08 21:48:28 -0700

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from'react';
+export default class Header extends Component{
+	render(){
+		return(
+			<div>
+				<header>
+		        	<div class="container">
+		            <div class="row">
+		                <div class="col-lg-12">
+		                    <img class="img-responsive" src="../../style/assets/Header.jpg" alt=""/>
+		                   
+		                </div>
+		            </div>
+		            </div>
+	            </header>
+        	</div>
+		)
+	}
+}
--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import NavigationBar from './NavigationBar'
 import SearchBar from './SearchBar'
 import Weekend from './Weekend'
+import Header from './Header'
 export default class App extends Component {
   render() {
     return (
       <div>
       	<NavigationBar />
-      	<div><img/></div>
+      	<Header />
       	<SearchBar />
       	<Weekend />
       </div>
